698c24d3f55b9ec0a5eb1e512435d8f7c84e595b
newdemian <dodemian@gmail.com>
2018-09-12 16:42:16 +0900

    3

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,10 +1,10 @@
 for i in range(100):
  j=i+1
- if (j%3==0):
+ if (0==j%15):
+  print('FizzBuzz')
+ elif (j%3==0):
   print('Fizz')
  elif (0==j%5):
   print('Buzz')
- elif (0==j%15):
-  print('FizzBuzz')
  else:
   print(j)
